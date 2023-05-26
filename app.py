f1ce11b15805cd2e6355a76ba3e3d9489f32ca30
Kerlon <111249129+kerlonfernandes@users.noreply.github.com>
2023-05-26 14:50:38 -0300

    email envio

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from email.message import EmailMessage
 from secret import senha
 
 #Configura email, senha
-EMAIL_ADDRESS = 'zzmids1@gmail.com'
+EMAIL_ADDRESS = '@gmail.com'
 EMAIL_PASSWORD = senha
 
 #Criar um e-mail
 msg = EmailMessage()
 msg['Subject'] = 'Olá'
 msg['From'] = "zzmids1@gmail.com" 
-msg['To'] = ['andrep16082004@gmail.com', 'kerlon1221@gmail.com', 'weldsakira@gmail.com', 'batatrap@gmail.com']
-msg.set_content('olá, Amo vocês')
+msg['To'] = ['@gmail.com']
+msg.set_content('Olá, este é um exemplo de email')
 
 
 #Enviar email
